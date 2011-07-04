f1dd35a4765a4ec7b03c3e968dbb2e4e734a8ad7
Guto Maia <guto@guto.net>
2011-07-04 17:24:24 -0300

    config.php

---
+++ config.php
@@ -0,0 +1,22 @@
+<?php
+
+define(CKEY, '');
+define(CSECRET, '');
+
+$cats = array(
+	'head' => array(
+		'key' => '',
+		'secret' => ''
+	),
+	'body' => array(
+		'key' => '',
+		'secret' => ''
+	),
+	'tail' => array(
+		'key' => '',
+		'secret' => ''
+	)
+);
+
+define(RECAPTCHA_PUBLIC, '');
+define(RECAPTCHA_PRIVATE, '');
